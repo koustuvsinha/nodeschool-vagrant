d7e1731e79bee245bea55a0313a53a00a492a718
Koustuv Sinha <koustuvsinha@hotmail.com>
2016-05-19 21:30:14 +0530

    Uncomment the installation lines

--- provision.sh
+++ provision.sh
@@ -15,26 +15,26 @@ echo "---------------------------------------------"
 
 # install heroku toolbelt
 echo "-------------- Installing heroku toolbelt -------------------------"
-#wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 # These shell script snippets are directly taken from heroku installation script
 # We want to avoid the apt-get update
 # add heroku repository to apt
 echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
 # install heroku's release key for package verification
-#wget -O- https://toolbelt.heroku.com/apt/release.key 2>&1 | apt-key add -
+wget -O- https://toolbelt.heroku.com/apt/release.key 2>&1 | apt-key add -
 
 echo "---------------------------------------------"
 echo "------- Updating package dependencies -------"
 echo "---------------------------------------------"
-#apt-get update -y # no need for sudo, and -y is needed to bypass the yes-no
+apt-get update -y # no need for sudo, and -y is needed to bypass the yes-no
 
 echo "---------------------------------------------"
 echo "-------- Installing packages ----------------"
 echo "---------------------------------------------"
 # install gcc and g++ and other build basics to ensure most software works
-#apt-get install -y --no-install-recommends heroku-toolbelt build-essential
+apt-get install -y --no-install-recommends heroku-toolbelt build-essential
 # install the cli
-#su - vagrant -c "heroku --version > /dev/null 2>&1"
+su - vagrant -c "heroku --version > /dev/null 2>&1"
 
 echo "---------------------------------------------"
 echo "-------- Installing NVM & Node --------------"
